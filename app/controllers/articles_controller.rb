9792a3530d826165b14ba800c7d23b77b5ff682d
nyshawnburton16@gmail.com <nburton@admins-MacBook-Pro-3.local>
2018-02-27 16:35:09 -0500

    added show action to article view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def new
     end
 
@@ -7,6 +11,11 @@ class ArticlesController < ApplicationController
 
         @article.save
         redirect_to @article
+    end
+
+    private
+        def article_params
+            params.require(:article).permit(:title,:text)
     end
 
 end
